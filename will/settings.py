f3bec20e5965750395c388c02367c0470e3b68ce
William Habsburg <william.habsburg@mohawkcollege.ca>
2022-08-31 13:52:45 -0400

    database error

--- will/settings.py
+++ will/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-le2@d4i$ug@mnd@=lrge4#g!xa!db&1#8iy87gn-g82a)-$4hg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
